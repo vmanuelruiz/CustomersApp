aacc46170393fa60f868e3446dceeacf1795a391
vmanuelruiz <vmruiz.ruiz@gmail.com>
2018-09-17 11:09:37 +0200

    cambio 4: instalando y usando libreria Link

---
+++ customers-app/src/components/CustomersActions.js
@@ -0,0 +1,19 @@
+//rscp = stateless component
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CustomersActions = ({children}) => {
+    return (
+        <div>
+            <div className="customers-actions">
+                <div>{children}</div>
+            </div>
+        </div>
+    );
+};
+
+CustomersActions.propTypes = {
+    children: PropTypes.node.isRequired, //todo tipo de elemento renderizable
+};
+
+export default CustomersActions;
