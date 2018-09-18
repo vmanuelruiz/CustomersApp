2f896e948b6e84913379f1e02aeec2d795f82e0d
vmanuelruiz <vmruiz.ruiz@gmail.com>
2018-09-18 13:01:46 +0200

    cambio 23: ruteo dinamico y propiedad children

--- customers-app/src/containers/CustomerContainer.js
+++ customers-app/src/containers/CustomerContainer.js
@@ -3,14 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import { getCustomerByDni } from './../selectors/customers';
+import { Route } from 'react-router-dom';
 
 class CustomerContainer extends Component {
+    //<p>Datos del cliente: {this.props.customer.name}</p>
+    renderBody = () => (
+        //El sig route india que si la URL hace match con el path, match se pone en TRUE y se agrega el tag p dependiendo si es o no edicion
+        <Route path="/customers/:dni/edit" children={
+            ({ match }) => ( match ? <p>Es edicion..</p> : <p>No es edicion..</p>)
+        } />
+    )
+
     render() {
         return (
             <div>
                 <AppFrame 
                     header={`Cliente: ${this.props.dni}`} 
-                    body={<p>Datos del cliente: {this.props.customer.name}</p>}>
+                    body={this.renderBody()}>
                 </AppFrame>
             </div>
         );
--- customers-app/src/selectors/customers.js
+++ customers-app/src/selectors/customers.js
@@ -4,5 +4,5 @@ export const getCustomers = state => state.customers;
 
 export const getCustomerByDni = createSelector (
     (state, props) => state.customers.find( c => c.dni === props.dni),
-    customer=> customer //aqui especifico que devuekvo un customer que recibira mapStateToProps de CustomerCOntainer
+    customer=> customer //aqui especifico que devuelvo un customer que recibira mapStateToProps de CustomerCOntainer
 );
