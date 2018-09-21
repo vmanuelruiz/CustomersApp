9d046818408acbfc6ae155fda682d38a0cf98de9
vmanuelruiz <vmruiz.ruiz@gmail.com>
2018-09-21 10:00:33 +0200

    cambio 37: insert customer con POST

--- customers-app/src/api/index.js
+++ customers-app/src/api/index.js
@@ -7,3 +7,22 @@ export const apiPut = (url, id, obj) => () =>
         body: JSON.stringify(obj),
         headers: new Headers({ 'Content-type': 'application/json' })
     }).then(v => v.json())
+    .then(r => {
+        if(r.error){
+            return Promise.reject(r.validation);
+        }
+        return r;
+    });
+
+export const apiPost = (url, obj) => () => 
+    fetch(`${url}`, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: new Headers({'Content-type': 'application/json'})
+    }).then(v => v.json())
+    .then(r => {
+        if(r.error){
+            return Promise.reject(r.validation);
+        }
+        return r;
+    });
--- customers-app/src/containers/CustomerContainer.js
+++ customers-app/src/containers/CustomerContainer.js
@@ -9,6 +9,7 @@ import CustomerEdit from './../components/CustomerEdit';
 import CustomerData from './../components/CustomerData';
 import {fetchCustomers} from './../actions/fetchCustomers';
 import {updateCustomer} from './../actions/updateCustomer';
+import { SubmissionError } from "redux-form";
 
 class CustomerContainer extends Component {
 
@@ -25,7 +26,11 @@ class CustomerContainer extends Component {
     handleSubmit = values => {
         console.log(JSON.stringify(values));
         const { id } = values;
-        return this.props.updateCustomer(id, values); //hago return aqui xq esto recibe una promise y necesito devolverla para 
+        return this.props.updateCustomer(id, values).then( r => {
+            if(r.error){
+                throw new SubmissionError(r.payload);
+            }
+        }); //hago return aqui xq esto recibe una promise y necesito devolverla para 
         //que el boton aceptar se bloquee mientras la promise no se termine de realizar.
     }
 
